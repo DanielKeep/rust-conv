e7014dd2548f4281e551d027bb4e852843852dec
Daniel Keep <daniel.keep@gmail.com>
2015-08-07 21:37:58 +1000

    Added notice on possible future API changes.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ This crate provides a number of conversion traits with more specific semantics t
 The goal with the traits provided here is to be more specific about what generic code can rely on, as well as provide reasonably self-describing alternatives to the standard `From`/`Into` traits.  For example, the although `T: From<U>` might be satisfied in generic code, this says nothing about what *kind* of conversion that represents.
 
 In addition, `From`/`Into` provide no facility for a conversion failing, meaning that implementations may need to choose between conversions that may not be valid, or panicking; neither option is appealing in general.
+
+# API Stability Notice
+
+The API of this crate is still not entirely decided.  In particular, errors may change in the future to carry the value that failed to convert (allowing it to be recovered).
 */
 
 #![deny(missing_docs)]
