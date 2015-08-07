f46c85c5cff8a6c347f1acd07d1957671d8580ee
Daniel Keep <daniel.keep@gmail.com>
2015-08-07 19:53:15 +1000

    Added docs for TryFrom.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ This crate provides a number of conversion traits with more specific semantics t
 #![deny(missing_docs)]
 
 // Exported macros.
-mod macros;
+pub mod macros;
 
 pub use errors::{
     NoError, Underflow, Overflow,
